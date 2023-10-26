93332f9208596779eef90dbd51e061ff6f93154e
Jefferson Formigoni <jeffersonformigoni86@gmail.com>
2023-10-26 19:01:26 -0300

    commit3

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   // baseURL: 'http://localhost:3000',
-  baseURL: 'https://foodexplorer-api-74ue.onrender.com',
+  baseURL: 'https://food-explorer-backend-main.onrender.com/',
 })
